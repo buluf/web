4c37536aacfba15c10a20142cf306b761e21317d
Leonardo Rothier Soares Cardoso <57650030+buluf@users.noreply.github.com>
2021-12-05 23:57:06 -0300

    Add files via upload

---
+++ javascripts/git.js
@@ -0,0 +1,31 @@
+function ProfileData() {
+  let xhr = new XMLHttpRequest()
+
+  xhr.onload = function () {        
+
+      let data = JSON.parse(this.responseText)
+
+      let perfilData = `
+      <div class="col-lg-4">
+    </div>
+      <div class="col-lg-8">
+      <h2>Leonardo Rothier</h2>
+      <p><strong>Descrição</strong> <br>
+        24 anos<br>
+        Graduanda em Ciência da Computação pela PUC-MG<br>
+        EStagiario<br>
+        <br>
+        <strong>Profile</strong>: ${data.name}<br>
+        <strong>Followers</strong>: ${data.followers}     <strong>Following</strong>: ${data.following}<br>
+        <strong>Repositories</strong>: ${data.public_repos}
+      </p>
+      <p>
+        <br>
+        </div>`;
+      
+      document.getElementById('git').innerHTML = perfilData;
+  }
+
+  xhr.open('GET', 'https://api.github.com/users/buluf');
+  xhr.send();
+}
